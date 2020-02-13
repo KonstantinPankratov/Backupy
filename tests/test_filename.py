013bfd22115fcaf8bf9bc0f5afa1349a9997b23d
KonstantinPankratov <hello@kopa.pw>
2020-02-13 08:57:42 +0100

    Changed file formation

--- tests/test_filename.py
+++ tests/test_filename.py
@@ -3,11 +3,7 @@ from datetime import datetime
 
 
 def test_backup_directory():
-    backup = Backupy()
-    assert backup.set_date_format('%Y.%m.%a') == '%Y.%m.%a'
-
+    backup = Backupy(filename_prefix='-backup', filename_format='{date}{prefix}', date_format='%Y.%m.%a')
     current_date = datetime.today().strftime(backup.date_format)
-    assert backup.set_filename() == 'backup_' + current_date
 
-    backup.set_filename_prefix('backup--')
-    assert backup.set_filename() == 'backup--' + current_date
+    assert backup.filename == current_date + '-backup'
